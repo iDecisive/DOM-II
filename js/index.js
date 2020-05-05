ac541f34bdde2fffd6ed6abbaeea9bb6201b0fd9
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-05 19:39:36 -0400

    Setup less and JS functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+let mouseOver = event => {
+
+
+}
+
+let keyDown = event => {
+
+    
+}
+
+let wheel = event => {
+
+    
+}
+
+let dragDrop = event => {
+
+    
+}
+
+let load = event => {
+
+    
+}
+
+let focus = event => {
+
+    
+}
+
+let resize = event => {
+
+    
+}
+
+let scroll = event => {
+
+    
+}
+
+let select = event => {
+
+    
+}
+
+let dblClick = event => {
+
+    
+}
+
