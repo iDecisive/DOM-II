5badbd69f4f6e5c10b9f920da00ce0a56e7a6a6c
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-06 07:31:28 -0400

    Added mouseover and keydown functionality

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@
 
 let mouseOver = event => {
 
+    event.target.style.filter = "invert(100%)";
 
 }
+
+document.querySelector(".intro img").addEventListener("mouseover", mouseOver);
 
 let keyDown = event => {
 
-    
+    if (document.querySelector(".logo-heading").style.transform === "rotate(180deg)") {
+
+        document.querySelector(".logo-heading").style.transform = "rotate(360deg)"
+
+    }else {
+
+        document.querySelector(".logo-heading").style.transform = "rotate(180deg)";
+
+    }
 }
+
+document.addEventListener("keydown", keyDown);
 
 let wheel = event => {
 
